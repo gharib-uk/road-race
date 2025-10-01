b55f5721f8ca78c163df02d01005f3e2af2cd32d
Alireza Gharib <alirezagharib110@gmail.com>
2025-10-01 09:22:16 +0330

    Keyboard Input is implemented

--- src/main.rs
+++ src/main.rs
@@ -56,5 +56,21 @@ fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
     }
 
     let player = engine.sprites.get_mut("player").unwrap();
-    player.translation.x += 100.0 * engine.delta_f32;
+
+    const MOVEMENT_SPEED:f32 = 100.0;
+    if engine.keyboard_state.pressed_any(&[KeyCode::Up, KeyCode::W]) {
+        player.translation.y += MOVEMENT_SPEED * engine.delta_f32;
+    }
+
+    if engine.keyboard_state.pressed_any(&[KeyCode::Down, KeyCode::S]) {
+        player.translation.y -= MOVEMENT_SPEED * engine.delta_f32;
+    }
+
+    if engine.keyboard_state.pressed_any(&[KeyCode::Right, KeyCode::D]) {
+        player.translation.x += MOVEMENT_SPEED * engine.delta_f32;
+    }
+
+    if engine.keyboard_state.pressed_any(&[KeyCode::Left, KeyCode::A]) {
+        player.translation.y -= MOVEMENT_SPEED * engine.delta_f32;
+    }
 }
